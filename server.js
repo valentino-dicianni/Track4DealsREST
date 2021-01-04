a3bd5afe0d1324c30d5935665f4e247783035c7a
“danielegerbaldo” <daniele.gerbaldo@edu.unito.it>
2021-01-04 06:33:26 -0800

    Mongodb connection string env variable added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/test', { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(
         () => { console.log('Connected to DB') },
         err => { console.log('ERROR connecting to db: ' + err) }
